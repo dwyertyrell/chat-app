309e7c3c253d8995bb58a0c822da6decb4fe6bb5
dwyertyrell <Dwyertyrell@gmail.com>
2025-06-29 17:34:32 +0100

    Integrate GiftedChat for chat UI and update scripts
    Replaced the static chat view with the GiftedChat component to provide a functional chat interface. Added initial message state and onSend handler. Updated package.json to include react-native-gifted-chat and related dependencies, and modified.

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,51 @@
 import { StyleSheet, Text, View } from "react-native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
 
 
 const Chat = ({route, navigation}) => {
-
+  const [messages, setMessages] = useState([]);
   const {name} = route.params
+  const onSend = (newMessages) => {
+    setMessages((previousMessage)=> {
+      GiftedChat.append(previousMessage, newMessages)
+    })
+  }
 
+  useEffect(()=>{
+
+    navigation.setOptions({title: name});
+  }, [])
+  
   useEffect(() => {
-    navigation.setOptions({title: name})
+
+    setMessages([
+      {
+        id: 1,
+        text: 'hello developer',
+        createdAt: new Date,
+        user: {
+          id: 2,
+          name: 'tyrell dwyer',
+          avatar: 'https://placeholder.com'
+        },
+      }
+    ])
+
   }, [])
   return (
-      <View style={styles.container}>
-        <Text>{name}</Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text>{name}</Text>
+      // </View>
+      
+      // this componenet came from the imported module, along with its own props
+    <GiftedChat
+      messages={messages}
+      onSend={onSend}
+      user={{
+        id:1
+      }}
+    />
     )
 };
 
