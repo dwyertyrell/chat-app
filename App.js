136e048a6624fc8776ca497d32aedd6bfa195f6d
dwyertyrell <Dwyertyrell@gmail.com>
2025-07-24 14:25:26 +0100

    Update App.js
    updated commented text

--- App.js
+++ App.js
@@ -1,3 +1,58 @@
+import Start from './components/Start';
+import Chat from './components/Chat';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth"; // <-- CHANGE HERE!
+
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'; // <-- NEW IMPORT!
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAH2zD62rjsghuJ2vnOF4nzooUl-osS5sg",
+    authDomain: "chat-app-a12bb.firebaseapp.com",
+    projectId: "chat-app-a12bb",
+    storageBucket: "chat-app-a12bb.firebasestorage.app",
+    messagingSenderId: "789584008757",
+    appId: "1:789584008757:web:fe3e7818039fbecf87837c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
+  // Initialize Firebase Auth with React Native persistence!
+  const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  });
+
+  return (
+    <NavigationContainer >
+      <Stack.Navigator
+        initialRouteName= 'Start'
+      >
+        <Stack.Screen
+          name='Start'
+        >
+          {props => <Start {...props} auth={auth} />}
+        </Stack.Screen>
+        <Stack.Screen
+          name='Chat'
+        >
+          {props => <Chat {...props} db={db} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
 // import Start from './components/Start';
 // import Chat from './components/Chat';
 // import { NavigationContainer } from '@react-navigation/native';
@@ -53,60 +108,3 @@
 //     </NavigationContainer>
 //   );
 // }
-
-
-
-
-import Start from './components/Start';
-import Chat from './components/Chat';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth"; // <-- CHANGE HERE!
-
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'; // <-- NEW IMPORT!
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAH2zD62rjsghuJ2vnOF4nzooUl-osS5sg",
-    authDomain: "chat-app-a12bb.firebaseapp.com",
-    projectId: "chat-app-a12bb",
-    storageBucket: "chat-app-a12bb.firebasestorage.app",
-    messagingSenderId: "789584008757",
-    appId: "1:789584008757:web:fe3e7818039fbecf87837c"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-
-  // Initialize Firebase Auth with React Native persistence!
-  const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
-
-  return (
-    <NavigationContainer >
-      <Stack.Navigator
-        initialRouteName= 'Start'
-      >
-        <Stack.Screen
-          name='Start'
-        >
-          {props => <Start {...props} auth={auth} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name='Chat'
-        >
-          {props => <Chat {...props} db={db} />}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
